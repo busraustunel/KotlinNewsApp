681bf749f940b932f6f017717cfee43cd6b1c45a
Büşra Üstünel <69143203+busraustunel@users.noreply.github.com>
2023-08-26 14:35:25 +0300

    add signup controls

--- app/src/main/java/com/example/newsapp/data/state/UserRegisterState.kt
+++ app/src/main/java/com/example/newsapp/data/state/UserRegisterState.kt
@@ -3,6 +3,8 @@ package com.example.newsapp.data.state
 sealed class UserRegisterState {
     object Idle:UserRegisterState()
     object Checking:UserRegisterState()
-    object Success:UserRegisterState()
+
+    object InputError:UserRegisterState()
+    class Success(val id:Int? = null):UserRegisterState()
     class Error(val throwable: Throwable? = null): UserRegisterState()
 }
--- app/src/main/java/com/example/newsapp/ui/signUp/SignUpFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/signUp/SignUpFragment.kt
@@ -2,6 +2,7 @@ package com.example.newsapp.ui.signUp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -13,61 +14,88 @@ import androidx.navigation.fragment.findNavController
 import com.example.newsapp.R
 import com.example.newsapp.data.state.UserRegisterState
 import com.example.newsapp.databinding.FragmentSignUpBinding
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
 
-    private val viewModel:SignUpViewModel by activityViewModels()
+    private val viewModel: SignUpViewModel by activityViewModels()
     lateinit var binding: FragmentSignUpBinding
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentSignUpBinding.bind(view)
         super.onViewCreated(view, savedInstanceState)
 
         observeRegisterState()
-        listeners()
+        setupListeners()
     }
 
     private fun observeRegisterState() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.CREATED) {
                 viewModel.registerState.collect {
-                    when(it) {
+                    when (it) {
                         UserRegisterState.Idle -> {}
-                        UserRegisterState.Checking -> {
-                            binding.progressBar.isVisible = true
-                            binding.btnSignUp.isVisible = false
-                        }
-                        UserRegisterState.Success -> {
-                            binding.progressBar.isVisible = false
-                            binding.btnSignUp.isVisible = true
-                            AlertDialog.Builder(requireContext())
-                                .setTitle("Success")
-                                .setMessage("User registered successfully")
-                                .setPositiveButton("Ok") { _, _ ->
-                                    findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
-                                }
-                                .show()
-                        }
-                        is UserRegisterState.Error -> {}
-
+                        UserRegisterState.Checking -> showLoadingState()
+                        is UserRegisterState.Success -> showSuccessState()
+                        UserRegisterState.InputError -> showInputError()
+                        is UserRegisterState.Error -> showErrorState()
                     }
                 }
             }
         }
     }
 
-    private fun listeners() {
+    private fun setupListeners() {
         binding.btnSignUp.setOnClickListener {
-            viewModel.insert(binding.etName.text.toString().trim(),
+            viewModel.insertUser(
+                binding.etName.text.toString().trim(),
                 binding.etSurname.text.toString().trim(),
                 binding.etEmail.text.toString().trim(),
                 binding.etPassword.text.toString().trim(),
                 binding.etPasswordAgain.text.toString().trim()
-                )
+            )
+        }
+
+        binding.btnGoLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
         }
     }
 
+    private fun showLoadingState() {
+        binding.progressBar.isVisible = true
+        binding.btnSignUp.isVisible = false
+    }
 
+    private fun showSuccessState() {
+        binding.progressBar.isVisible = false
+        binding.btnSignUp.isVisible = true
+
+        //TODO: Bu mesaj neden iki kere gösteriliyor her gidip geldiğimde?
+        Toast.makeText(requireContext(), "Kayıt başarılı", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showInputError() {
+        if (binding.etName.text.isEmpty() || binding.etSurname.text.isEmpty() || binding.etEmail.text.isEmpty() || binding.etPassword.text.isEmpty() || binding.etPasswordAgain.text.isEmpty()) Toast.makeText(
+            requireContext(),
+            "Lütfen tüm alanları doldurun",
+            Toast.LENGTH_SHORT
+        ).show()
+        else if (binding.etPassword.text.toString() != binding.etPasswordAgain.text.toString()) Toast.makeText(
+            requireContext(),
+            "Şifreler eşleşmiyor",
+            Toast.LENGTH_SHORT
+        ).show()
+        else if (binding.etPassword.text.length < 6) Toast.makeText(
+            requireContext(),
+            "Şifre en az 6 karakter olmalı",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun showErrorState() {
+        binding.progressBar.isVisible = false
+        binding.btnSignUp.isVisible = true
+        Toast.makeText(requireContext(), "Bir hata oluştu", Toast.LENGTH_SHORT).show()
+    }
 }
